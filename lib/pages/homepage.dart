71fef6a27c73ba8b77ac2101b3527e924de725e1
Ikhwan Ash Shafa Arrafi <ikhwanfredy@gmail.com>
2022-10-17 01:54:21 +0700

    Ngerubah Tampilan Besar2an

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_application_2/auth.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
 
@@ -18,11 +20,11 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _title(){
-    return const Text('Selamat Datang');
+    return const Text('Welcome Euy');
   }
 
   Widget _userUid(){
-    return Text(user?.email ?? 'ID Warga');
+    return Text(user?.email?? 'ID Warga');
   }
 
   Widget _signOutButton(){
@@ -35,22 +37,191 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context){
   return Scaffold(
-    appBar: AppBar(
-      title: _title(),
-    ),
-    body: Container(
-      height: double.infinity,
-      width: double.infinity,
-      padding: const EdgeInsets.all(20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          _userUid(),
-          _signOutButton(),
-        ],
-      ),
-    ),
+    body: SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Padding(padding: EdgeInsetsDirectional.fromSTEB(20, 0, 16, 10),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Card(
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  color: const Color(0xFF4B39EF),
+                  elevation: 2,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(40),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(2, 2, 2, 2),
+                    child: Container(
+                      width:60,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                      ),
+                      child: Image.asset('assets/images/ringojuice.jpg',
+                      fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Align(
+                    alignment: AlignmentDirectional(0,0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(20, 0, 20, 0),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.max,
+                            children: <Widget>[
+                              _userUid(),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(20, 0, 20, 0),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              Text(
+                                'Selamat Datang!'
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    )
+                  ),
+                )
+              ],
+            ),
+            ),
+            Padding(
+              padding: EdgeInsetsDirectional.fromSTEB((0), 0, 0, 15),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children:[
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(0, 0, 150, 0),
+                        child: Text(
+                          'Kabar Sekitar Anda',
+                          textAlign: TextAlign.start,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 0),
+                        child: Container(
+                          width: 350,
+                          height: 100,
+                          decoration: BoxDecoration(
+                            color: Color(0xFF9B99B9),
+                            borderRadius: BorderRadius.circular(8),
+                            shape: BoxShape.rectangle,
+                          ),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              Padding(
+                                padding: EdgeInsetsDirectional.fromSTEB(0, 20, 200, 0),
+                                child: Text(
+                                  'Informasi',
+                                  textAlign: TextAlign.start,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ]
+                  )
+                ],
+              ),
+            ),
+            Wrap(
+              spacing: 8,
+              runSpacing: 8,
+              alignment: WrapAlignment.start,
+              crossAxisAlignment: WrapCrossAlignment.start,
+              direction: Axis.horizontal,
+              runAlignment: WrapAlignment.start,
+              verticalDirection: VerticalDirection.down,
+              clipBehavior: Clip.none,
+              children: [
+                Container(
+                  width: 160,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF4B39EF),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 3,
+                        color: Color(0x33000000),
+                        offset: Offset(0,1),
+                      )
+                    ],
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(12, 8, 12, 8),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 12),
+                          child: FaIcon(
+                            FontAwesomeIcons.moneyCheckDollar,
+                            color: Colors.white,
+                            size: 44,
+                          ),
+                        ),
+                        Text(
+                          'Laporan Keuangan',
+                          /*style: FlutterFlowTheme.of(context)
+                          .bodyText2
+                          .override(
+                            fontFamily: 'Outfit',
+                            color: Colors.white,
+                            fontWeight: FontWeight.normal,
+                          ),*/
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 160,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF4B39EF),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 3,
+                        color: Color(0x33000000),
+                        offset: Offset(0,1),
+                      )
+                    ],
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      )
+    )
   );
 
 
